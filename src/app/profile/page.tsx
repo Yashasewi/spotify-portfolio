44f7168113ed67a961632e093f436e84869f9d84
Yashasewi <32950332+Yashasewi@users.noreply.github.com>
2023-07-08 13:05:48 +0530

    profile page started

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,24 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 function page() {
-  return <div>page</div>;
+  return (
+    <>
+      <div>
+        <Image
+          className="rounded-full"
+          src="https://i.scdn.co/image/ab67757000003b82d008b7ccf168c1d105b9e6be"
+          alt="chutiya_hu_main"
+          width={200}
+          height={200}
+        />
+        <Link href={"#"}>
+          <h1>Yashasewi Singh</h1>
+        </Link>
+      </div>
+    </>
+  );
 }
 
 export default page;
