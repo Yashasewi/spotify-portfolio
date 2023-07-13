ebb4b02f6ba37fb1a9a6dc1d3b4e1e10bd93e7e8
Yashasewi <Yashasewisingh2668@gmail.com>
2023-07-14 00:51:19 +0530

    adding servers session

--- src/app/(functions)/layout.tsx
+++ src/app/(functions)/layout.tsx
@@ -1,17 +1,18 @@
+"use client";
 import Nav from "@/components/Nav";
-import { loginIsRequiredServer } from "@/utils/auth";
+import { SessionProvider } from "next-auth/react";
 
-export default async function DashboardLayout({
+export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
-  }) {
-  await loginIsRequiredServer();
-  
+}) {
   return (
-    <div>
-      <Nav />
-      <div className="w-[100%] pl-[6rem]">{children}</div>
-    </div>
+    <SessionProvider>
+      <div>
+        <Nav />
+        <div className="w-[100%] pl-[6rem]">{children}</div>
+      </div>
+    </SessionProvider>
   );
 }
