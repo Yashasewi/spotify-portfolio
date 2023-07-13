bcbb1af61880e78aea59be1fad2f47cf5bc61254
Yashasewi <Yashasewisingh2668@gmail.com>
2023-07-14 01:11:26 +0530

    added cn func move function from auth to helper

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,32 +1,8 @@
-import { NextAuthOptions, getServerSession } from "next-auth";
+import { NextAuthOptions } from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
-import { redirect } from "next/navigation";
+import { refreshAccessToken } from "@/utils/helper";
 
-async function refreshAccessToken(token: any) {
-  const params = new URLSearchParams();
-  params.append("grant_type", "refresh_token");
-  params.append("refresh_token", token.refreshToken);
-  const response = await fetch("https://accounts.spotify.com/api/token", {
-    method: "POST",
-    headers: {
-      Authorization:
-        "Basic " +
-        Buffer.from(
-          process.env.SPOTIFY_CLIENT_ID +
-            ":" +
-            process.env.SPOTIFY_CLIENT_SECRET
-        ).toString("base64"),
-    },
-    body: params,
-  });
-  const data = await response.json();
-  return {
-    ...token,
-    accessToken: data.access_token,
-    refreshToken: data.refresh_token ?? token.refreshToken,
-    accessTokenExpires: Date.now() + data.expires_in * 1000,
-  };
-}
+
 
 export const authCOnfig: NextAuthOptions = {
   providers: [
@@ -73,8 +49,3 @@ export const authCOnfig: NextAuthOptions = {
     },
   },
 };
-
-export async function loginIsRequiredServer() {
-  const session = await getServerSession(authCOnfig);
-  if (!session) return redirect("/");
-}
