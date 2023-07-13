3b3cb7341fb95060ad8a262a2a0c25b5bfb15bc1
Yashasewi <32950332+Yashasewi@users.noreply.github.com>
2023-07-14 01:11:26 +0530

    added cn func move function from auth to helper

---
+++ src/utils/helper.ts
@@ -0,0 +1,32 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export async function refreshAccessToken(token: any) {
+  const params = new URLSearchParams();
+  params.append("grant_type", "refresh_token");
+  params.append("refresh_token", token.refreshToken);
+  const response = await fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
+    headers: {
+      Authorization:
+        "Basic " +
+        Buffer.from(
+          process.env.SPOTIFY_CLIENT_ID +
+            ":" +
+            process.env.SPOTIFY_CLIENT_SECRET
+        ).toString("base64"),
+    },
+    body: params,
+  });
+  const data = await response.json();
+  return {
+    ...token,
+    accessToken: data.access_token,
+    refreshToken: data.refresh_token ?? token.refreshToken,
+    accessTokenExpires: Date.now() + data.expires_in * 1000,
+  };
+}
