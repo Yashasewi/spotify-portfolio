ea217dfdef37e53c5f0cf2bf7386ef80251e7022
Yashasewi <Yashasewisingh2668@gmail.com>
2023-07-14 00:51:58 +0530

    editing auth ts for token and callback

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -2,16 +2,75 @@ import { NextAuthOptions, getServerSession } from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
 import { redirect } from "next/navigation";
 
+async function refreshAccessToken(token: any) {
+  const params = new URLSearchParams();
+  params.append("grant_type", "refresh_token");
+  params.append("refresh_token", token.refreshToken);
+  const response = await fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
+    headers: {
+      Authorization:
+        "Basic " +
+        Buffer.from(
+          process.env.SPOTIFY_CLIENT_ID +
+            ":" +
+            process.env.SPOTIFY_CLIENT_SECRET
+        ).toString("base64"),
+    },
+    body: params,
+  });
+  const data = await response.json();
+  return {
+    ...token,
+    accessToken: data.access_token,
+    refreshToken: data.refresh_token ?? token.refreshToken,
+    accessTokenExpires: Date.now() + data.expires_in * 1000,
+  };
+}
+
 export const authCOnfig: NextAuthOptions = {
   providers: [
     SpotifyProvider({
       clientId: process.env.SPOTIFY_CLIENT_ID as string,
       clientSecret: process.env.SPOTIFY_CLIENT_SECRET as string,
+      authorization: {
+        url: "https://accounts.spotify.com/authorize",
+        params: {
+          scope:
+            "user-read-email user-read-private user-read-playback-state user-read-recently-played user-top-read user-library-read playlist-read-private streaming app-remote-control user-follow-read user-follow-modify playlist-read-private playlist-read-collaborative playlist-modify-public user-read-private",
+        },
+      },
     }),
   ],
-
+  secret: process.env.NEXTAUTH_SECRET,
   pages: {
-    signIn: "/signin",
+    signIn: "/",
+  },
+  callbacks: {
+    async jwt({ token, account }) {
+      // Persist the OAuth access_token to the token right after signin
+      if (account) {
+        token.accessToken = account.access_token;
+        token.refreshToken = account.refresh_token;
+        token.accessTokenExpires = account.expires_at;
+        return token;
+      }
+      // access token has not expired
+      if (
+        token.accessTokenExpires &&
+        Date.now() < token.accessTokenExpires * 1000
+      ) {
+        return token;
+      }
+
+      // access token has expired
+      return await refreshAccessToken(token);
+    },
+    async session({ session, token, user }) {
+      // Send properties to the client, like an access_token from a provider.
+      session.accessToken = token.accessToken;
+      return session;
+    },
   },
 };
 
@@ -19,25 +78,3 @@ export async function loginIsRequiredServer() {
   const session = await getServerSession(authCOnfig);
   if (!session) return redirect("/");
 }
-// export function LoginIsRequiredClient() {
-//   if (typeof window === "undefined") {
-//     const session = useSession();
-//     const router = useRouter();
-//     if (!session) return router.push("/signin");
-//   }
-// }
-
-// function loginIsRequiredClient() {
-//   // Check if the function is running in a serverless environment.
-//   if (typeof window === "undefined") {
-//     // Get the session and router objects.
-//     const session = useSession();
-//     const router = useRouter();
-
-//     // If the session is empty, redirect the user to the sign-in page.
-//     if (!session) return router.push("/signin");
-//   }
-
-//   // The user is logged in.
-//   return false;
-// }
