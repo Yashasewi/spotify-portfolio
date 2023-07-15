a1b0eb65a50486ac78c38f5cd537291b1f960c8a
Yashasewi <32950332+Yashasewi@users.noreply.github.com>
2023-07-15 12:01:18 +0530

    doing spotify profile page [working]

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,4 +1,4 @@
-import { NextAuthOptions } from "next-auth";
+import { NextAuthOptions, getServerSession } from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
 import { refreshAccessToken } from "@/utils/helper";
 
@@ -49,3 +49,5 @@ export const authCOnfig: NextAuthOptions = {
     },
   },
 };
+
+export const getAuthSession = () => getServerSession(authCOnfig);
