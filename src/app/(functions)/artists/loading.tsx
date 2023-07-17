6154e8c4de88b7e7f96b838b3aa389955deb6f3f
Yashasewi <Yashasewisingh2668@gmail.com>
2023-07-17 17:24:26 +0530

    added lading screen to  artist and playlist page

--- src/app/(functions)/artists/loading.tsx
+++ src/app/(functions)/artists/loading.tsx
@@ -1,7 +1,120 @@
+import { Skeleton } from "@/components/Skeleton";
+
 export default function loading() {
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <div className="lds-hourglass"></div>
+    <div className="mx-8">
+      <div className="px-16 py-12">
+        <header className="flex justify-between px-5 ">
+          <Skeleton className="w-32 h-4 ml-3" />
+
+          <div className="flex gap-x-3 ">
+            <Skeleton className="w-20 h-3" />
+            <Skeleton className="w-20 h-3" />
+            <Skeleton className="w-20 h-3" />
+          </div>
+        </header>
+
+        <section>
+          <div className="grid grid-cols-5 gap-4 mt-12">
+            <div className="flex flex-col items-center justify-center text-center gap-3 p-2">
+              <div className="inline-block w-[200px] h-[200px]">
+                <div className="rounded-full  aspect-square">
+                  <Skeleton className="w-[200px] h-[200px] rounded-full" />
+                </div>
+              </div>
+              <Skeleton className="w-24 h-4" />
+            </div>
+            <div className="flex flex-col items-center justify-center text-center gap-3 p-2">
+              <div className="inline-block w-[200px] h-[200px]">
+                <div className="rounded-full  aspect-square">
+                  <Skeleton className="w-[200px] h-[200px] rounded-full" />
+                </div>
+              </div>
+              <Skeleton className="w-24 h-4" />
+            </div>
+            <div className="flex flex-col items-center justify-center text-center gap-3 p-2">
+              <div className="inline-block w-[200px] h-[200px]">
+                <div className="rounded-full  aspect-square">
+                  <Skeleton className="w-[200px] h-[200px] rounded-full" />
+                </div>
+              </div>
+              <Skeleton className="w-24 h-4" />
+            </div>
+            <div className="flex flex-col items-center justify-center text-center gap-3 p-2">
+              <div className="inline-block w-[200px] h-[200px]">
+                <div className="rounded-full  aspect-square">
+                  <Skeleton className="w-[200px] h-[200px] rounded-full" />
+                </div>
+              </div>
+              <Skeleton className="w-24 h-4" />
+            </div>
+            <div className="flex flex-col items-center justify-center text-center gap-3 p-2">
+              <div className="inline-block w-[200px] h-[200px]">
+                <div className="rounded-full  aspect-square">
+                  <Skeleton className="w-[200px] h-[200px] rounded-full" />
+                </div>
+              </div>
+              <Skeleton className="w-24 h-4" />
+            </div>
+            <div className="flex flex-col items-center justify-center text-center gap-3 p-2">
+              <div className="inline-block w-[200px] h-[200px]">
+                <div className="rounded-full  aspect-square">
+                  <Skeleton className="w-[200px] h-[200px] rounded-full" />
+                </div>
+              </div>
+              <Skeleton className="w-24 h-4" />
+            </div>
+            <div className="flex flex-col items-center justify-center text-center gap-3 p-2">
+              <div className="inline-block w-[200px] h-[200px]">
+                <div className="rounded-full  aspect-square">
+                  <Skeleton className="w-[200px] h-[200px] rounded-full" />
+                </div>
+              </div>
+              <Skeleton className="w-24 h-4" />
+            </div>
+            <div className="flex flex-col items-center justify-center text-center gap-3 p-2">
+              <div className="inline-block w-[200px] h-[200px]">
+                <div className="rounded-full  aspect-square">
+                  <Skeleton className="w-[200px] h-[200px] rounded-full" />
+                </div>
+              </div>
+              <Skeleton className="w-24 h-4" />
+            </div>
+            <div className="flex flex-col items-center justify-center text-center gap-3 p-2">
+              <div className="inline-block w-[200px] h-[200px]">
+                <div className="rounded-full  aspect-square">
+                  <Skeleton className="w-[200px] h-[200px] rounded-full" />
+                </div>
+              </div>
+              <Skeleton className="w-24 h-4" />
+            </div>
+            <div className="flex flex-col items-center justify-center text-center gap-3 p-2">
+              <div className="inline-block w-[200px] h-[200px]">
+                <div className="rounded-full  aspect-square">
+                  <Skeleton className="w-[200px] h-[200px] rounded-full" />
+                </div>
+              </div>
+              <Skeleton className="w-24 h-4" />
+            </div>
+            <div className="flex flex-col items-center justify-center text-center gap-3 p-2">
+              <div className="inline-block w-[200px] h-[200px]">
+                <div className="rounded-full  aspect-square">
+                  <Skeleton className="w-[200px] h-[200px] rounded-full" />
+                </div>
+              </div>
+              <Skeleton className="w-24 h-4" />
+            </div>
+            <div className="flex flex-col items-center justify-center text-center gap-3 p-2">
+              <div className="inline-block w-[200px] h-[200px]">
+                <div className="rounded-full  aspect-square">
+                  <Skeleton className="w-[200px] h-[200px] rounded-full" />
+                </div>
+              </div>
+              <Skeleton className="w-24 h-4" />
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
--- src/app/(functions)/playlist/loading.tsx
+++ src/app/(functions)/playlist/loading.tsx
@@ -1,7 +1,116 @@
+import { Skeleton } from "@/components/Skeleton";
+
 export default function loading() {
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <div className="lds-hourglass"></div>
+    <div className="mx-8">
+      <div className="px-16 py-12">
+        <header className="flex justify-between px-5 ">
+          <Skeleton className="w-32 h-4 ml-3" />
+        </header>
+
+        <section>
+          <div className="grid grid-cols-5 gap-4 mt-12">
+            <div className="flex flex-col items-center justify-center text-center gap-y-3 p-2">
+              <Skeleton className="w-[230px] h-[230px] rounded-none" />
+              <span className="flex flex-col gap-y-2  items-center justify-center">
+                <Skeleton className="w-[160px] h-[10px]" />
+                <Skeleton className="w-[100px] h-[8px]" />
+              </span>
+            </div>
+            <div className="flex flex-col items-center justify-center text-center gap-y-3 p-2">
+              <Skeleton className="w-[230px] h-[230px] rounded-none" />
+              <span className="flex flex-col gap-y-2  items-center justify-center">
+                <Skeleton className="w-[160px] h-[10px]" />
+                <Skeleton className="w-[100px] h-[8px]" />
+              </span>
+            </div>
+            <div className="flex flex-col items-center justify-center text-center gap-y-3 p-2">
+              <Skeleton className="w-[230px] h-[230px] rounded-none" />
+              <span className="flex flex-col gap-y-2  items-center justify-center">
+                <Skeleton className="w-[160px] h-[10px]" />
+                <Skeleton className="w-[100px] h-[8px]" />
+              </span>
+            </div>
+            <div className="flex flex-col items-center justify-center text-center gap-y-3 p-2">
+              <Skeleton className="w-[230px] h-[230px] rounded-none" />
+              <span className="flex flex-col gap-y-2  items-center justify-center">
+                <Skeleton className="w-[160px] h-[10px]" />
+                <Skeleton className="w-[100px] h-[8px]" />
+              </span>
+            </div>
+            <div className="flex flex-col items-center justify-center text-center gap-y-3 p-2">
+              <Skeleton className="w-[230px] h-[230px] rounded-none" />
+              <span className="flex flex-col gap-y-2  items-center justify-center">
+                <Skeleton className="w-[160px] h-[10px]" />
+                <Skeleton className="w-[100px] h-[8px]" />
+              </span>
+            </div>
+            <div className="flex flex-col items-center justify-center text-center gap-y-3 p-2">
+              <Skeleton className="w-[230px] h-[230px] rounded-none" />
+              <span className="flex flex-col gap-y-2  items-center justify-center">
+                <Skeleton className="w-[160px] h-[10px]" />
+                <Skeleton className="w-[100px] h-[8px]" />
+              </span>
+            </div>
+            <div className="flex flex-col items-center justify-center text-center gap-y-3 p-2">
+              <Skeleton className="w-[230px] h-[230px] rounded-none" />
+              <span className="flex flex-col gap-y-2  items-center justify-center">
+                <Skeleton className="w-[160px] h-[10px]" />
+                <Skeleton className="w-[100px] h-[8px]" />
+              </span>
+            </div>
+            <div className="flex flex-col items-center justify-center text-center gap-y-3 p-2">
+              <Skeleton className="w-[230px] h-[230px] rounded-none" />
+              <span className="flex flex-col gap-y-2  items-center justify-center">
+                <Skeleton className="w-[160px] h-[10px]" />
+                <Skeleton className="w-[100px] h-[8px]" />
+              </span>
+            </div>
+            <div className="flex flex-col items-center justify-center text-center gap-y-3 p-2">
+              <Skeleton className="w-[230px] h-[230px] rounded-none" />
+              <span className="flex flex-col gap-y-2  items-center justify-center">
+                <Skeleton className="w-[160px] h-[10px]" />
+                <Skeleton className="w-[100px] h-[8px]" />
+              </span>
+            </div>
+            <div className="flex flex-col items-center justify-center text-center gap-y-3 p-2">
+              <Skeleton className="w-[230px] h-[230px] rounded-none" />
+              <span className="flex flex-col gap-y-2  items-center justify-center">
+                <Skeleton className="w-[160px] h-[10px]" />
+                <Skeleton className="w-[100px] h-[8px]" />
+              </span>
+            </div>
+            <div className="flex flex-col items-center justify-center text-center gap-y-3 p-2">
+              <Skeleton className="w-[230px] h-[230px] rounded-none" />
+              <span className="flex flex-col gap-y-2  items-center justify-center">
+                <Skeleton className="w-[160px] h-[10px]" />
+                <Skeleton className="w-[100px] h-[8px]" />
+              </span>
+            </div>
+            <div className="flex flex-col items-center justify-center text-center gap-y-3 p-2">
+              <Skeleton className="w-[230px] h-[230px] rounded-none" />
+              <span className="flex flex-col gap-y-2  items-center justify-center">
+                <Skeleton className="w-[160px] h-[10px]" />
+                <Skeleton className="w-[100px] h-[8px]" />
+              </span>
+            </div>
+            <div className="flex flex-col items-center justify-center text-center gap-y-3 p-2">
+              <Skeleton className="w-[230px] h-[230px] rounded-none" />
+              <span className="flex flex-col gap-y-2  items-center justify-center">
+                <Skeleton className="w-[160px] h-[10px]" />
+                <Skeleton className="w-[100px] h-[8px]" />
+              </span>
+            </div>
+            <div className="flex flex-col items-center justify-center text-center gap-y-3 p-2">
+              <Skeleton className="w-[230px] h-[230px] rounded-none" />
+              <span className="flex flex-col gap-y-2  items-center justify-center">
+                <Skeleton className="w-[160px] h-[10px]" />
+                <Skeleton className="w-[100px] h-[8px]" />
+              </span>
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
