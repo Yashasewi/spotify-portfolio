00bfbc2e0d6431fc5c73a93c5efa164ee6f302bd
Yashasewi <32950332+Yashasewi@users.noreply.github.com>
2023-07-17 15:20:54 +0530

    image hostname configure

--- next.config.js
+++ next.config.js
@@ -1,15 +1,33 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   images: {
+    // domains: [
+    //   "i.scdn.co",
+    //   "t.scdn.co",
+    //   "newjams-images.scdn.co",
+    //   "dailymix-images.scdn.co",
+    //   "seed-mix-image.spotifycdn.com",
+    //   "charts-images.scdn.co",
+    //   "daily-mix.scdn.co",
+    //   "mixed-media-images.spotifycdn.com",
+    //   "scdn.co",
+    //   "mosaic.scdn.co",
+    //   "wrapped-images.spotifycdn.com",
+    //   "blend-playlist-covers.spotifycdn.com",
+    //   "seeded-session-images.scdn.co",
+    // ],
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "i.scdn.co",
-        port: "",
-        pathname: "/image/**",
+        hostname: "**.scdn.co",
+      },
+      {
+        protocol: "https",
+        hostname: "**.spotifycdn.com",
       },
     ],
   },
+
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
