168df53bd80132918c7962f39f504ce7532ceb2b
Yashasewi <32950332+Yashasewi@users.noreply.github.com>
2023-07-17 17:23:32 +0530

    added home loading screen

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+export default function Loading() {
+  return (
+    <div className="">
+      <div className="flex flex-col items-center justify-center h-screen gap-2">
+        <h1 className="text-4xl font-bold animate-pulse">
+          Preparing your Profile...
+        </h1>
+      </div>
+    </div>
+  );
+}
