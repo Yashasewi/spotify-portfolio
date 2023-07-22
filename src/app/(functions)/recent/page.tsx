4ffefd10a30c5fcc307d87e80c6b0c4a73654de3
Yashasewi <Yashasewisingh2668@gmail.com>
2023-07-22 21:56:51 +0530

    removing console logs some bug fixing

--- src/app/(functions)/recent/page.tsx
+++ src/app/(functions)/recent/page.tsx
@@ -1,94 +1,89 @@
 import Image from "next/image";
 import Link from "next/link";
 import { getAuthSession } from "@/utils/auth";
-import { artistToString, millisecondsToMinutes } from "@/utils/helper";
+import {
+    artistToString,
+    getLastPlayedTime,
+    millisecondsToMinutes,
+} from "@/utils/helper";
 
 async function Recent() {
-  const session = await getAuthSession();
+    const session = await getAuthSession();
 
-  const response = await fetch(
-    `https://api.spotify.com/v1/me/player/recently-played?limit=50`,
-    {
-      headers: {
-        Authorization: `Bearer ${session!.accessToken}`,
-      },
-    }
-  );
+    const response = await fetch(
+        `https://api.spotify.com/v1/me/player/recently-played?limit=50`,
+        {
+            headers: {
+                Authorization: `Bearer ${session!.accessToken}`,
+            },
+        }
+    );
 
-  const data = await response.json();
-  const recentItems = data.items;
+    const data = await response.json();
+    const recentItems = data.items;
 
-  // console.log(recentItems[0]);
+    // console.log(recentItems[0]);
+    // console.log(recentItems[0].track.id);
 
-  function getLastPlayedTime(date: string | Date): string {
-    const currentDate = new Date();
-    const parsedDate = typeof date === "string" ? new Date(date) : date;
-    const timeDifference = currentDate.getTime() - parsedDate.getTime();
-    const millisecondsInHour = 1000 * 60 * 60;
-    const millisecondsInDay = millisecondsInHour * 24;
-
-    if (timeDifference < millisecondsInDay) {
-      // Less than 24 hours
-      const hours = Math.floor(timeDifference / millisecondsInHour);
-      return hours + " hours ago";
-    } else {
-      // More than 24 hours
-      const days = Math.floor(timeDifference / millisecondsInDay);
-      return days + " days ago";
-    }
-  }
-
-  return (
-    <div>
-      <div className="mx-8 ">
-        <div className="px-14 py-12 pb-8">
-          <header className="flex justify-between px-2  ">
-            <h1 className="text-xl font-semibold">Recent</h1>
-          </header>
-        </div>
-
-        <section>
-          <div className="flex flex-col px-16 gap-y-8 my-4 mt-2">
-            {recentItems.map((track: any) => (
-              <Link
-                href={`/tracks/` + track.id}
-                key={track.id}
-                className="flex items-center gap-x-7 font-medium"
-              >
-                <Image
-                  className=" "
-                  src={track.track.album.images[0].url}
-                  alt="Artist Image"
-                  width={60}
-                  height={60}
-                />
-                <div className="flex justify-between w-[100%]">
-                  <div>
-                    <h3 className="text-lg font-medium">{track.track.name}</h3>
-                    <p className="text-sm text-gray-300 opacity-70">
-                      <span className="mr-2">
-                        {artistToString(track.track.album.artists)}{" "}
-                      </span>
-                      {"·"}{" "}
-                      <span className="ml-2">{track.track.album.name}</span>
-                    </p>
-                  </div>
-                  <span className="mr-12 flex flex-col justify-center gap-y-1">
-                    <p className="text-sm text-gray-300 opacity-70">
-                      {millisecondsToMinutes(track.track.duration_ms)}
-                    </p>
-                    <p className="text-xs text-gray-300 opacity-70">
-                      {getLastPlayedTime(track.played_at)}
-                    </p>
-                  </span>
+    return (
+        <div>
+            <div className="mx-8 ">
+                <div className="px-14 py-12 pb-8">
+                    <header className="flex justify-between px-2  ">
+                        <h1 className="text-xl font-semibold">Recent</h1>
+                    </header>
                 </div>
-              </Link>
-            ))}
-          </div>
-        </section>
-      </div>
-    </div>
-  );
+
+                <section>
+                    <div className="flex flex-col px-16 gap-y-8 my-4 mt-2">
+                        {recentItems.map((track: any) => (
+                            <Link
+                                href={`/tracks/` + track.track.id}
+                                key={track.id}
+                                className="flex items-center gap-x-7 font-medium"
+                            >
+                                <Image
+                                    className=" "
+                                    src={track.track.album.images[0].url}
+                                    alt="Artist Image"
+                                    width={60}
+                                    height={60}
+                                />
+                                <div className="flex justify-between w-[100%]">
+                                    <div>
+                                        <h3 className="text-lg font-medium">
+                                            {track.track.name}
+                                        </h3>
+                                        <p className="text-sm text-gray-300 opacity-70">
+                                            <span className="mr-2">
+                                                {artistToString(
+                                                    track.track.album.artists
+                                                )}{" "}
+                                            </span>
+                                            {"·"}{" "}
+                                            <span className="ml-2">
+                                                {track.track.album.name}
+                                            </span>
+                                        </p>
+                                    </div>
+                                    <span className="mr-12 flex flex-col justify-center gap-y-1">
+                                        <p className="text-sm text-gray-300 opacity-70">
+                                            {millisecondsToMinutes(
+                                                track.track.duration_ms
+                                            )}
+                                        </p>
+                                        <p className="text-xs text-gray-300 opacity-70">
+                                            {getLastPlayedTime(track.played_at)}
+                                        </p>
+                                    </span>
+                                </div>
+                            </Link>
+                        ))}
+                    </div>
+                </section>
+            </div>
+        </div>
+    );
 }
 
 export default Recent;
--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -2,61 +2,79 @@ import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+    return twMerge(clsx(inputs));
 }
 
 export async function refreshAccessToken(token: any) {
-  const params = new URLSearchParams();
-  params.append("grant_type", "refresh_token");
-  params.append("refresh_token", token.refreshToken);
-  const response = await fetch("https://accounts.spotify.com/api/token", {
-    method: "POST",
-    headers: {
-      Authorization:
-        "Basic " +
-        Buffer.from(
-          process.env.SPOTIFY_CLIENT_ID +
-            ":" +
-            process.env.SPOTIFY_CLIENT_SECRET
-        ).toString("base64"),
-    },
-    body: params,
-  });
+    const params = new URLSearchParams();
+    params.append("grant_type", "refresh_token");
+    params.append("refresh_token", token.refreshToken);
+    const response = await fetch("https://accounts.spotify.com/api/token", {
+        method: "POST",
+        headers: {
+            Authorization:
+                "Basic " +
+                Buffer.from(
+                    process.env.SPOTIFY_CLIENT_ID +
+                        ":" +
+                        process.env.SPOTIFY_CLIENT_SECRET
+                ).toString("base64"),
+        },
+        body: params,
+    });
 
-  //  1689760004851
-  //  1689763425 + 3600 = 1689767025
-  const data = await response.json();
-  console.log("response from refresh token", data);
-  return {
-    ...token,
-    accessToken: data.access_token,
-    accessTokenExpires: Date.now() / 1000 + (data.expires_in as number),
-  };
+    //  1689760004851
+    //  1689763425 + 3600 = 1689767025
+    const data = await response.json();
+    // console.log("response from refresh token", data);
+    return {
+        ...token,
+        accessToken: data.access_token,
+        accessTokenExpires: Date.now() / 1000 + (data.expires_in as number),
+    };
 }
 
 export async function getSpotifyProfile(accessToken: string) {
-  const res = await fetch("https://api.spotify.com/v1/me", {
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-    },
-  });
-  return await res.json();
+    const res = await fetch("https://api.spotify.com/v1/me", {
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+        },
+    });
+    return await res.json();
 }
 
 export const millisecondsToMinutes = (ms: number) => {
-  const minutes = Math.floor(ms / 60000);
-  const seconds = ((ms % 60000) / 1000).toFixed(0);
-  return `${minutes}:${+seconds < 10 ? "0" : ""}${seconds}`;
+    const minutes = Math.floor(ms / 60000);
+    const seconds = ((ms % 60000) / 1000).toFixed(0);
+    return `${minutes}:${+seconds < 10 ? "0" : ""}${seconds}`;
 };
 
 export const dateToYear = (date: string) => {
-  return new Date(date).getFullYear();
+    return new Date(date).getFullYear();
 };
 
 export const artistToString = (artists: any[]) => {
-  return artists.map((artist) => artist.name).join(" , ");
+    return artists.map((artist) => artist.name).join(" , ");
 };
 
 export const truncate = (str: string, n: number = 30) => {
-  return str.length > n ? str.substr(0, n - 1) + "..." : str;
+    return str.length > n ? str.substr(0, n - 1) + "..." : str;
 };
+
+export function getLastPlayedTime(date: string | Date): string {
+    const currentDate = new Date();
+    const parsedDate = typeof date === "string" ? new Date(date) : date;
+    const timeDifference = currentDate.getTime() - parsedDate.getTime();
+    const millisecondsInHour = 1000 * 60 * 60;
+    const millisecondsInDay = millisecondsInHour * 24;
+
+    if (timeDifference < millisecondsInDay) {
+        // Less than 24 hours
+        const hours = Math.floor(timeDifference / millisecondsInHour);
+        return hours + " hours ago";
+    } else {
+        // More than 24 hours
+        const days = Math.floor(timeDifference / millisecondsInDay);
+        return days + " days ago";
+    }
+}
