cb29db9978630b2256bdb66bbc7afe2254c4f9a1
Yashasewi <32950332+Yashasewi@users.noreply.github.com>
2023-07-23 22:42:19 +0530

    error handling for no items present

--- src/app/(functions)/artists/page.tsx
+++ src/app/(functions)/artists/page.tsx
@@ -4,21 +4,23 @@ import Artist from "@/components/Artist";
 // * This is the code for the Top Artists Page
 
 export default function ArtistPage() {
-  // TODO: Add a Time Range Selector for Top Artists (All Time, Last 6 Months, Last 4 Weeks)
+    // TODO: Add a Time Range Selector for Top Artists (All Time, Last 6 Months, Last 4 Weeks)
 
-  return (
-    <div className="mx-8">
-      <div className="px-16 py-12">
-        <header className="flex justify-between px-5 ">
-          <h1 className="text-xl font-semibold">Top Artists</h1>
-          <div className="text-base flex gap-x-3">
-            <span className="underline underline-offset-4">All Time</span>
-            <span>Last 6 Month</span>
-            <span>Last 4 Weeks</span>
-          </div>
-        </header>
-        <Artist />
-      </div>
-    </div>
-  );
+    return (
+        <div className="mx-8">
+            <div className="px-16 py-12">
+                <header className="flex justify-between px-5 ">
+                    <h1 className="text-xl font-semibold">Top Artists</h1>
+                    <div className="text-base  gap-x-3 hidden">
+                        <span className="underline underline-offset-4">
+                            All Time
+                        </span>
+                        <span>Last 6 Month</span>
+                        <span>Last 4 Weeks</span>
+                    </div>
+                </header>
+                <Artist />
+            </div>
+        </div>
+    );
 }
--- src/components/Artist.tsx
+++ src/components/Artist.tsx
@@ -1,62 +1,70 @@
 import { Artist } from "@/types/types";
 import { SpotifyApi } from "@/utils/SpotifyApi";
 import { getAuthSession } from "@/utils/auth";
-import { StaticImport } from "next/dist/shared/lib/get-img-props";
 import Image from "next/image";
 import Link from "next/link";
-import {
-  Key,
-  ReactElement,
-  JSXElementConstructor,
-  ReactNode,
-  ReactPortal,
-  PromiseLikeOfReactNode,
-} from "react";
-import { UrlObject } from "url";
 
 const Artist = async () => {
-  const session = await getAuthSession();
+    const session = await getAuthSession();
 
-  const spotifyApi = new SpotifyApi(session!.accessToken);
+    const spotifyApi = new SpotifyApi(session!.accessToken);
 
-  const topArtists = await spotifyApi.getMyTopArtists({
-    limit: 50,
-    time_range: "long_term",
-  });
+    const topArtists = await spotifyApi.getMyTopArtists({
+        limit: 50,
+        time_range: "long_term",
+    });
 
-  const topArtistsItems = topArtists.items;
-  return (
-    <section>
-      <div className="grid grid-cols-5 gap-4 mt-12">
-        {topArtistsItems.map((artist: Artist) => (
-          <div
-            className="flex flex-col items-center justify-center text-center gap-3 p-2"
-            key={artist.id}
-          >
-            <Link
-              className="inline-block w-[200px] h-[200px]"
-              href={`/artists/${artist.id}`}
-            >
-              <Image
-                className="rounded-full  aspect-square"
-                src={artist.images[0].url}
-                alt="Profile Picture"
-                width={200}
-                height={200}
-              />
-            </Link>
-            <Link
-              href={artist.external_urls.spotify}
-              target="_blank"
-              className=""
-            >
-              {artist.name}
-            </Link>
-          </div>
-        ))}
-      </div>
-    </section>
-  );
+    const topArtistsItems = topArtists.items;
+    if (topArtistsItems.length == 0)
+        return (
+            <div className="flex flex-col items-center justify-center  text-center  p-12">
+                <p>
+                    You dont have any top artists yet. Please listen to some
+                    music.
+                </p>
+                <p>
+                    <a
+                        href="https://open.spotify.com/"
+                        target="_blank"
+                        className="underline cursor-pointer text-green-500"
+                    >
+                        Open Player
+                    </a>
+                </p>
+            </div>
+        );
+    return (
+        <section>
+            <div className="grid grid-cols-5 gap-4 mt-12">
+                {topArtistsItems.map((artist: Artist) => (
+                    <div
+                        className="flex flex-col items-center justify-center text-center gap-3 p-2"
+                        key={artist.id}
+                    >
+                        <Link
+                            className="inline-block w-[200px] h-[200px]"
+                            href={`/artists/${artist.id}`}
+                        >
+                            <Image
+                                className="rounded-full  aspect-square"
+                                src={artist.images[0].url}
+                                alt="Profile Picture"
+                                width={200}
+                                height={200}
+                            />
+                        </Link>
+                        <Link
+                            href={artist.external_urls.spotify}
+                            target="_blank"
+                            className=""
+                        >
+                            {artist.name}
+                        </Link>
+                    </div>
+                ))}
+            </div>
+        </section>
+    );
 };
 
 export default Artist;
