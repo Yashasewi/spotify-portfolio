1adfffd49ccccbfe02dc43148ebe632c002a711d
Yashasewi <32950332+Yashasewi@users.noreply.github.com>
2023-07-24 17:29:10 +0530

    making artist and profile loading responsive

--- src/app/(functions)/artists/page.tsx
+++ src/app/(functions)/artists/page.tsx
@@ -7,8 +7,8 @@ export default function ArtistPage() {
     // TODO: Add a Time Range Selector for Top Artists (All Time, Last 6 Months, Last 4 Weeks)
 
     return (
-        <div className="mx-8">
-            <div className="px-16 py-12">
+        <div className="sm:mx-8 ">
+            <div className="px-8 sm:px-16 py-12">
                 <header className="flex justify-between px-5 ">
                     <h1 className="text-xl font-semibold">Top Artists</h1>
                     <div className="text-base  gap-x-3 hidden">
