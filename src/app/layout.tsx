44141570b3fb5a60e041ba5135933fad9f5a20bb
Yashasewi <32950332+Yashasewi@users.noreply.github.com>
2023-08-02 01:15:20 +0530

    clean up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,42 +19,6 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <title>Spotify Stats</title>
-            <meta name="title" content="Spotify Stats" />
-            <meta
-                name="description"
-                content="A web app for visualizing personalized Spotify data built with React, Express, and the Spotify API "
-            />
-
-            <meta property="og:type" content="website" />
-            <meta
-                property="og:url"
-                content="https://spotify.yashasewi.eu.org/"
-            />
-            <meta property="og:title" content="Spotify Stats" />
-            <meta
-                property="og:description"
-                content="A web app for visualizing personalized Spotify data built with React, Express, and the Spotify API "
-            />
-            <meta
-                property="og:image"
-                content="https://metatags.io/images/meta-tags.png"
-            />
-
-            <meta property="twitter:card" content="summary_large_image" />
-            <meta
-                property="twitter:url"
-                content="https://spotify.yashasewi.eu.org/"
-            />
-            <meta property="twitter:title" content="Spotify Stats" />
-            <meta
-                property="twitter:description"
-                content="A web app for visualizing personalized Spotify data built with React, Express, and the Spotify API "
-            />
-            <meta
-                property="twitter:image"
-                content="https://metatags.io/images/meta-tags.png"
-            />
             <body className={rubik.className}>
                 <div>{children}</div>
             </body>
