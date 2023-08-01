1b2b26a703436454721550f2cb5b74455e5fa4b0
Yashasewi <32950332+Yashasewi@users.noreply.github.com>
2023-08-02 00:30:29 +0530

    Added the images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,25 +2,27 @@ import "./globals.css";
 import { Rubik } from "next/font/google";
 
 export const rubik = Rubik({
-  subsets: ["latin"],
-  weight: ["400", "500"],
+    subsets: ["latin"],
+    weight: ["400", "500"],
 });
 export const metadata = {
-  title: "Spotify",
-  description:
-    "A web app for visualizing personalized Spotify data built with React, Express, and the Spotify API ",
+    title: "Spotify Stats",
+    description:
+        "A web app for visualizing personalized Spotify data built with React, Express, and the Spotify API ",
+  url: "https://spotify.yashasewi.eu.org",
+  image: "https://spotify.yashasewi.eu.org/spotify-stats.png",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={rubik.className}>
-        <div>{children}</div>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={rubik.className}>
+                <div>{children}</div>
+            </body>
+        </html>
+    );
 }
